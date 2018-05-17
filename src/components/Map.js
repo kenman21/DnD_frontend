142e5ef8651aa1e147602497ab21927d3689973a
Kenny Lehr <kenman9009@gmail.com>
2018-05-17 10:41:05 -0400

    Setup tilemap with divs

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,28 +10,33 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
+    this.createCanvasLines()
+    }
+
+  createCanvasLines = () => {
     let canvas = document.getElementById('canvas-1')
-    canvas.width = 820;
-    canvas.height= 820;
     // Visible Canvas Width
-    let map_w = 800;
-    let map_h = 800;
+    let map_res = 50
+    let pixel_size = 16
+    let map_hw = pixel_size * map_res;
     let p = 10;
+    canvas.width = map_hw+2*p;
+    canvas.height= map_hw+2*p;
     let ctx = canvas.getContext('2d');
-    for (var x = 0; x <= map_w; x += 16) {
+    for (var x = 0; x <= map_hw; x += 16) {
       ctx.moveTo(0.5 + x + p, p);
-      ctx.lineTo(0.5 + x + p, map_h + p);
+      ctx.lineTo(0.5 + x + p, map_hw + p);
       }
 
 
-    for (var x = 0; x <= map_h; x += 16) {
+    for (var x = 0; x <= map_hw; x += 16) {
         ctx.moveTo(p, 0.5 + x + p);
-        ctx.lineTo(map_w + p, 0.5 + x + p);
+        ctx.lineTo(map_hw + p, 0.5 + x + p);
       }
 
     ctx.strokeStyle = "grey";
     ctx.stroke();
-    }
+  }
 
   handleClick = () => {
     let canvas = document.getElementById('canvas-2')
@@ -45,14 +50,16 @@ class Map extends React.Component {
     ctx.drawImage(image,x,y,16,16,0,0,32,32);
   }
 
-  render() {
-    // <button onClick={this.handleClick}>Click This</button>
+  creategrid(l, w, class_name, multiplier=1) {
+    let grid_l = l
+    let grid_w = w
+
     const grid = []
-    for (let i = 0; i < 50; i++){
+    for (let i = 0; i < grid_l; i++){
       const row = []
-      for (let j = 0; j < 50; j++){
+      for (let j = 0; j < grid_w; j++){
         row.push(
-          <td key={`${i},${j}`} id={`${i},${j}`}>
+          <td className={class_name} key={`${i},${j}`} id={`${j*multiplier},${i*multiplier}`}>
           </td>
         )
       }
@@ -62,17 +69,31 @@ class Map extends React.Component {
         </tr>
       )
     }
+    return grid
+  }
+
+  render() {
 
     return (
       <div id="canvas-container">
         <canvas id="canvas-1" >
         </canvas>
-        <div className="table">
-          <table id="grid">
-            <tbody id="body">
-              { grid }
+        <div className="center table">
+          <table id="map-grid">
+            <tbody id="map-body">
+              { this.creategrid(50,50,"canvas-grid") }
             </tbody>
           </table>
+        </div>
+        <div className="right">
+          <img src="Page-1.png" alt="Page-1" id="Page-1"/>
+          <div className="right tileMap">
+            <table id="tileMap-grid">
+              <tbody id="tileMap-body">
+                { this.creategrid(16,16,"tile-grid", 16) }
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     )
